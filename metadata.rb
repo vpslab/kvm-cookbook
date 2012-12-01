4c04727a3a04cb7c7570518d81b38f79d5e0d70b
Paweł Pacana <pawel.pacana@gmail.com>
2012-12-01 22:12:31 +0100

    Initial release.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "VPSLab"
+maintainer_email "pawel.pacana@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures kvm"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+depends          "ntp"
+supports         "debian"
+supports         "ubuntu"
---
+++ recipes/host.rb
@@ -0,0 +1,4 @@
+include_recipe "ntp"
+
+package "qemu-kvm"
+package "ebtables"
